9ffd0ba9e44698703015e9eb53d8bca039233ae7
Jatin Gumber <93000878+Jatin-gumber12@users.noreply.github.com>
2024-03-09 10:24:42 +0530

    Initial Commit

--- app/src/main/java/com/example/hungryhub/StartActivity.kt
+++ app/src/main/java/com/example/hungryhub/StartActivity.kt
@@ -1,16 +1,25 @@
 package com.example.hungryhub
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hungryhub.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
