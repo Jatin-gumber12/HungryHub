64812f7082fac5df80f2168a07ba84001045f2d9
Jatin Gumber <93000878+Jatin-gumber12@users.noreply.github.com>
2024-03-10 17:10:02 +0530

    Login and Signin Page final

--- app/src/main/java/com/example/hungryhub/SignActivity.kt
+++ app/src/main/java/com/example/hungryhub/SignActivity.kt
@@ -1,16 +1,26 @@
 package com.example.hungryhub
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hungryhub.databinding.ActivityLoginBinding
+import com.example.hungryhub.databinding.ActivitySignBinding
 
 class SignActivity : AppCompatActivity() {
+    private val binding: ActivitySignBinding by lazy {
+        ActivitySignBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign)
+        setContentView(binding.root)
+        binding.alreadybutton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
