53eb79983b6852d75e710f7e52750825055f41ba
Jatin Gumber <93000878+Jatin-gumber12@users.noreply.github.com>
2024-03-10 21:17:24 +0530

    Location Page

---
+++ app/src/main/java/com/example/hungryhub/ChooseLocationActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hungryhub
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hungryhub.databinding.ActivityChooseLocationBinding
+
+class ChooseLocationActivity : AppCompatActivity() {
+    private val binding:ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
