30536446fdf3f34f1a65465cf4bd85143a8ff55e
Jatin Gumber <93000878+Jatin-gumber12@users.noreply.github.com>
2024-03-14 00:24:40 +0530

    Location Page

--- app/src/main/java/com/example/hungryhub/ChooseLocationActivity.kt
+++ app/src/main/java/com/example/hungryhub/ChooseLocationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hungryhub
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,7 +16,10 @@ class ChooseLocationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        val locationlist = arrayOf("jaipur")
+        val locationlist = arrayOf("Prem Nagar","Rajpur","Makkawala","Kirsali Gaon","Guniyal Gaon","FRI","ISBT","Malsi","Race Course","Garhi Cantt")
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,locationlist)
+        val autoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
