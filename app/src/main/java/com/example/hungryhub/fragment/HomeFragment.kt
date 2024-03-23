71e99501f89b5b2bfe05ecb918a2d9c27dacc005
Jatin Gumber <93000878+Jatin-gumber12@users.noreply.github.com>
2024-03-23 22:50:34 +0530

    Image added in fragment, popular and menu initiated

--- app/src/main/java/com/example/hungryhub/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/hungryhub/fragment/HomeFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.hungryhub.R
+import com.example.hungryhub.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 
@@ -16,6 +21,7 @@ import com.example.hungryhub.R
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +31,38 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+      //  return inflater.inflate(R.layout.fragment_home, container, false)
+
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.banner1,ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner22,ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner33,ScaleTypes.FIT))
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList,ScaleTypes.FIT)
+        imageSlider.setItemClickListener(object : ItemClickListener{
+            override fun doubleClick(position: Int) {
+
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMessage = "Selected Image $position"
+                Toast.makeText(requireContext(),itemMessage,Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
     companion object {
 
     }
