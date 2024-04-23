cde2ba4a5ace9ad5c26ea3790ff1134e63b78a5c
Jatin Gumber <93000878+Jatin-gumber12@users.noreply.github.com>
2024-04-24 01:45:54 +0530

    Order placed Done

---
+++ app/src/main/java/com/example/hungryhub/PayOutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hungryhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PayOutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_out)
+    }
+}
