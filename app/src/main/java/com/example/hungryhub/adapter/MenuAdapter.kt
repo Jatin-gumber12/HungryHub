abb0abd699922cdbc6b925dd2d7fb1ab87131182
Aman Nishad <75328768+javaman97@users.noreply.github.com>
2024-05-12 23:39:34 +0530

    Fark nhi

--- app/src/main/java/com/example/hungryhub/adapter/MenuAdapter.kt
+++ app/src/main/java/com/example/hungryhub/adapter/MenuAdapter.kt
@@ -1,32 +1,68 @@
 package com.example.hungryhub.adapter
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.hungryhub.databinding.MenuItemBinding
 
-class MenuAdapter(private val menuItems : MutableList<String>, private val menuItemPrice : MutableList<String>,private val menuImage : MutableList<Int>) : RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuAdapter.MenuViewHolder {
+class MenuAdapter(
+    private val menuItems: ArrayList<String>,
+    private val requireContext: ArrayList<String>,
+    arrayList: ArrayList<Int>,
+    requireContext1: Context
+) : RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
+    private val itemClickListener: OnClickListener?= null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
         val binding = MenuItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return MenuViewHolder(binding)
     }
-
-    override fun onBindViewHolder(holder: MenuAdapter.MenuViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
         holder.bind(position)
+
     }
 
     override fun getItemCount(): Int = menuItems.size
 
-    inner class MenuViewHolder(private val binding:MenuItemBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(position: Int) {
-            binding.apply{
-                MenuFoodName.text = menuItems[position]
-                MenuPrice.text = menuItemPrice[position]
-                MenuImage.setImageResource(menuImage[position])
+    inner class MenuViewHolder(private val binding: MenuItemBinding) :RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    openDetailActivity(position)
+                }
+                // setonclic listner to open details
+                val intent = Intent( requireContext, DetailsActivity::class.java)
+                intent.putExtra("MenuItemName", menuItemsName.get(position))
+                intent.putExtra("MenuItemImage", MenuImage.get(position))
+                intent.putExtra("MenuItemDescription", MenuDescriptions.get(position))
+                intent.putExtra("MenuItemIngredients", MenuIngredients.get(position))
+                intent.putExtra("MenuItemPrice", menuItemPrice.get(position))
+                requireContext.startActivity(intent)
             }
         }
 
+        private fun openDetailActivity(position: Int) {
+
+        }
+
+        fun bind(position: Int) {
+            binding.apply {
+                menuFoodName.text=menuItemsName[position]
+                menuPrice.text = menuItemPrice[position]
+                val uriString = MenuImage[position]
+                val uri = Uri.parse(uriString)
+                Glide.with(requireContext).load(uri).into(menuImage)
+
+//               menuImage.setImageResource(MenuImage[position])
+            }
+        }
     }
 
+    interface OnClickListener{
+        fun onItemClick(position: Int)
+    }
 }
+
---
+++ app/src/main/java/com/example/hungryhub/model/MenuItem.kt
@@ -0,0 +1,9 @@
+package com.example.hungryhub.model
+
+data class MenuItem(
+val foodName:String?=null,
+val foodPrice:String?=null,
+val foodDesc:String?=null,
+val foodImage:String?=null,
+val foodIngriedients:String?=null,
+)
